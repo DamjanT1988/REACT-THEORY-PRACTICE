f4cecea6c8dd8783aedc10e557aca8e6a4e8e0d2
DamjanT1988 <Damjan_t@hotmail.com>
2022-12-05 21:32:11 +0100

    1

--- src/index.js
+++ src/index.js
@@ -2429,3 +2429,264 @@ ReactDOM.render(
   <FlavorForm />,
   document.getElementById('root32')
 );*/
+
+
+//THE FILE INPUT TAG
+
+/*
+In HTML, an <input type="file"> lets the user choose one or more files from their device storage 
+to be uploaded to a server or manipulated by JavaScript via the File API.
+
+<input type="file" />
+
+Because its value is read-only, it is an uncontrolled component in React. It is discussed together 
+with other uncontrolled components later in the documentation.
+*/
+
+
+//HANDLING MULTIPLE INPUTS
+
+/*
+When you need to handle multiple controlled input elements, you can add a name attribute to each 
+element and let the handler function choose what to do based on the value of event.target.name.
+
+For example:
+
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value    
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+            name="isGoing"            
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"            
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>
+    );
+  }
+}
+
+Try it on CodePen
+
+Note how we used the ES6 computed property name syntax to update the state key corresponding to the given input name:
+
+this.setState({
+  [name]: value});
+
+It is equivalent to this ES5 code:
+
+var partialState = {};
+partialState[name] = value;this.setState(partialState);
+
+Also, since setState() automatically merges a partial state into the current state, we only needed to call it with the changed parts.
+*/
+
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>
+    );
+  }
+}
+
+
+const root33 = ReactDOM.createRoot(document.getElementById('root33'));
+root33.render(<Reservation />);
+
+
+//CONTROLLED INPUT NULL VALUE
+
+/*
+Specifying the value prop on a controlled component prevents the user from changing the input 
+unless you desire so. If you’ve specified a value but the input is still editable, you may have 
+accidentally set value to undefined or null.
+
+The following code demonstrates this. (The input is locked at first but becomes editable after 
+a short delay.)
+
+ReactDOM.createRoot(mountNode).render(<input value="hi" />);
+
+setTimeout(function() {
+  ReactDOM.createRoot(mountNode).render(<input value={null} />);
+}, 1000);
+*/
+
+
+//ALTERNATIVES TO CONTROLLED COMPONENTS
+
+/*
+It can sometimes be tedious to use controlled components, because you need to write an event 
+handler for every way your data can change and pipe all of the input state through a React 
+component. This can become particularly annoying when you are converting a preexisting codebase 
+to React, or integrating a React application with a non-React library. In these situations, 
+you might want to check out uncontrolled components, an alternative technique for implementing 
+input forms.
+*/
+
+
+//FULLY-FLEDGED SOLUTIONS
+
+/*
+If you’re looking for a complete solution including validation, keeping track of the visited 
+fields, and handling form submission, Formik is one of the popular choices. However, it is 
+built on the same principles of controlled components and managing state — so don’t neglect 
+to learn them.
+*/
+
+
+
+//************************************************************************************
+// LIFTING STATE UP
+//************************************************************************************
+
+/*
+Often, several components need to reflect the same changing data. We recommend lifting the 
+shared state up to their closest common ancestor. Let’s see how this works in action.
+
+In this section, we will create a temperature calculator that calculates whether the water 
+would boil at a given temperature.
+
+We will start with a component called BoilingVerdict. It accepts the celsius temperature as a 
+prop, and prints whether it is enough to boil the water:
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;  }
+  return <p>The water would not boil.</p>;}
+
+Next, we will create a component called Calculator. It renders an <input> that lets you enter 
+the temperature, and keeps its value in this.state.temperature.
+
+Additionally, it renders the BoilingVerdict for the current input value.
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {temperature: ''};  }
+
+  handleChange(e) {
+    this.setState({temperature: e.target.value});  }
+
+  render() {
+    const temperature = this.state.temperature;    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input          
+        value={temperature}          
+        onChange={this.handleChange} />        
+        <BoilingVerdict          
+        celsius={parseFloat(temperature)} />      
+        </fieldset>
+    );
+  }
+}
+
+Try it on CodePen
+*/
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {temperature: ''};
+  }
+
+  handleChange(e) {
+    this.setState({temperature: e.target.value});
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange} />
+        <BoilingVerdict
+          celsius={parseFloat(temperature)} />
+      </fieldset>
+    );
+  }
+}
+
+
+const root34 = ReactDOM.createRoot(document.getElementById('root34'));
+root34.render(<Calculator />);
