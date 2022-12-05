2a16eef01099aab8ec1f328eba3647a177133127
DamjanT1988 <Damjan_t@hotmail.com>
2022-12-05 19:56:43 +0100

    1

--- src/index.js
+++ src/index.js
@@ -2319,3 +2319,113 @@ class EssayForm extends React.Component {
 
 const root31 = ReactDOM.createRoot(document.getElementById('root31'));
 root31.render(<EssayForm />);
+
+
+//THE SELECT TAG
+
+/*
+In HTML, <select> creates a drop-down list. For example, this HTML creates a drop-down list of 
+flavors:
+
+<select>
+  <option value="grapefruit">Grapefruit</option>
+  <option value="lime">Lime</option>
+  <option selected value="coconut">Coconut</option>
+  <option value="mango">Mango</option>
+</select>
+
+Note that the Coconut option is initially selected, because of the selected attribute. React, 
+instead of using this selected attribute, uses a value attribute on the root select tag. This is 
+more convenient in a controlled component because you only need to update it in one place. 
+For example:
+
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {    
+    this.setState({value: event.target.value});  
+  }
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>            
+          <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+Try it on CodePen
+
+Overall, this makes it so that <input type="text">, <textarea>, and <select> all work very 
+similarly - they all accept a value attribute that you can use to implement a controlled component.
+
+    Note
+    You can pass an array into the value attribute, allowing you to select multiple options in 
+    a select tag:
+
+    <select multiple={true} value={['B', 'C']}>
+*/
+
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange3 = this.handleChange3.bind(this);
+    this.handleSubmit3 = this.handleSubmit3.bind(this);
+  }
+
+  handleChange3(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit3(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit3}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange3}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+const root32 = ReactDOM.createRoot(document.getElementById('root32'));
+root32.render(<FlavorForm />);
+
+/*
+ReactDOM.render(
+  <FlavorForm />,
+  document.getElementById('root32')
+);*/
