28dcf4da2d21354ca12704ec9815e8253edb00ad
DamjanT1988 <Damjan_t@hotmail.com>
2022-12-12 00:01:19 +0100

    1

--- src/index.js
+++ src/index.js
@@ -3971,6 +3971,37 @@ hierarchy:
             -ProductRow
 */
 
+
+//STEP 2: BUILD A STATIC VERSION IN REACT
+
+/*
+See the Pen Thinking In React: Step 2 on CodePen.
+
+Now that you have your component hierarchy, it’s time to implement your app. The easiest way is to 
+build a version that takes your data model and renders the UI but has no interactivity. It’s best 
+to decouple these processes because building a static version requires a lot of typing and no 
+thinking, and adding interactivity requires a lot of thinking and not a lot of typing. We’ll see 
+why.
+
+To build a static version of your app that renders your data model, you’ll want to build components 
+that reuse other components and pass data using props. props are a way of passing data from parent 
+to child. If you’re familiar with the concept of state, don’t use state at all to build this static 
+version. State is reserved only for interactivity, that is, data that changes over time. Since this 
+is a static version of the app, you don’t need it.
+
+You can build top-down or bottom-up. That is, you can either start with building the components 
+higher up in the hierarchy (i.e. starting with FilterableProductTable) or with the ones lower in 
+it (ProductRow). In simpler examples, it’s usually easier to go top-down, and on larger projects, 
+it’s easier to go bottom-up and write tests as you build.
+
+At the end of this step, you’ll have a library of reusable components that render your data model. 
+The components will only have render() methods since this is a static version of your app. The 
+component at the top of the hierarchy (FilterableProductTable) will take your data model as a 
+prop. If you make a change to your underlying data model and call root.render() again, the UI 
+will be updated. You can see how your UI is updated and where to make changes. React’s one-way 
+data flow (also called one-way binding) keeps everything modular and fast.
+*/
+
 //5 child to ProductTabe
 class ProductCategoryRow extends React.Component {
   render() {
@@ -4084,45 +4115,6 @@ root41.render(<FilterableProductTable products={PRODUCTS} />);
 
 
 
-//STEP 2: BUILD A STATIC VERSION IN REACT
-
-/*
-See the Pen Thinking In React: Step 2 on CodePen.
-
-Now that you have your component hierarchy, it’s time to implement your app. The easiest way is to 
-build a version that takes your data model and renders the UI but has no interactivity. It’s best 
-to decouple these processes because building a static version requires a lot of typing and no 
-thinking, and adding interactivity requires a lot of thinking and not a lot of typing. We’ll see 
-why.
-
-To build a static version of your app that renders your data model, you’ll want to build components 
-that reuse other components and pass data using props. props are a way of passing data from parent 
-to child. If you’re familiar with the concept of state, don’t use state at all to build this static 
-version. State is reserved only for interactivity, that is, data that changes over time. Since this 
-is a static version of the app, you don’t need it.
-
-You can build top-down or bottom-up. That is, you can either start with building the components 
-higher up in the hierarchy (i.e. starting with FilterableProductTable) or with the ones lower in 
-it (ProductRow). In simpler examples, it’s usually easier to go top-down, and on larger projects, 
-it’s easier to go bottom-up and write tests as you build.
-
-At the end of this step, you’ll have a library of reusable components that render your data model. 
-The components will only have render() methods since this is a static version of your app. The 
-component at the top of the hierarchy (FilterableProductTable) will take your data model as a 
-prop. If you make a change to your underlying data model and call root.render() again, the UI 
-will be updated. You can see how your UI is updated and where to make changes. React’s one-way 
-data flow (also called one-way binding) keeps everything modular and fast.
-
-Refer to the React docs if you need help executing this step.
-
-A Brief Interlude: Props vs State:
-There are two types of “model” data in React: props and state. It’s important to understand the 
-distinction between the two; skim the official React docs if you aren’t sure what the difference 
-is. See also FAQ: What is the difference between state and props?
-*/
-
-
-
 //STEP 3: IDENTIFY THE MINIMAL --BUT COMPLETE-- REPRESENTATION OF UI STATE
 
 /*
@@ -4202,7 +4194,6 @@ and set the values of the form fields in SearchBar.
 You can start seeing how your application will behave: set filterText to "ball" and refresh your 
 app. You’ll see that the data table is updated correctly.
 */
-
 
 //6
 class ProductCategoryRow2 extends React.Component {
